62b0dd9b7441a4d344976f286cbb5dd74505a694
tongzhaomei <tongzhaomei@wisight.cn>
2022-01-24 11:18:42 +0800

    imageview控件渲染远程url图片；

--- BasicAndroidAccessibility-Kotlin/app/src/main/java/com/example/android/basicandroidaccessibility/ui/home/HomeFragment.kt
+++ BasicAndroidAccessibility-Kotlin/app/src/main/java/com/example/android/basicandroidaccessibility/ui/home/HomeFragment.kt
@@ -1,22 +1,30 @@
 package com.example.android.basicandroidaccessibility.ui.home
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.android.basicandroidaccessibility.databinding.FragmentHomeBinding
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private var imageView: ImageView? = null  //图片
+    // 声明执行器来解析 URL
+    private val executor: ExecutorService = Executors.newSingleThreadExecutor()
+    // 该属性仅在 onCreateView 和onDestroyView
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -30,10 +38,34 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        // 文本内容
         val textView: TextView = binding.textHome
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+        // 图片内容
+        imageView = binding.imageView
+        // 一旦 executor 解析 URL 并接收到图像，处理程序将在 ImageView 中加载它
+        val handler = Handler(Looper.getMainLooper())
+        // 初始化图像
+        var image: Bitmap? = null
+        // 仅用于后台进程（可能需要时间，具体取决于 Internet 速度）
+        executor.execute{
+            // Image URL
+            val imageURL = "https://media.geeksforgeeks.org/wp-content/cdn-uploads/gfg_200x200-min.png"
+            try {
+                val `in` = java.net.URL(imageURL).openStream()
+                image = BitmapFactory.decodeStream(`in`)
+                // 仅用于在 UI 中进行更改
+                handler.post {
+                    imageView!!.setImageBitmap(image)
+                }
+            }
+            // 如果 URL 不指向图像或任何其他类型的故障
+            catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
         return root
     }
 
