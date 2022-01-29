8ce48981d1ddacd7b24994190cd73e4a2a82cc2c
tongzhaomei <1003632308@qq.com>
2022-01-29 21:31:02 +0800

    新增表单并验证输入内容；

--- BasicAndroidAccessibility-Kotlin/app/src/main/java/com/example/android/basicandroidaccessibility/activity/TemplateActivity.kt
+++ BasicAndroidAccessibility-Kotlin/app/src/main/java/com/example/android/basicandroidaccessibility/activity/TemplateActivity.kt
@@ -5,15 +5,14 @@ package com.example.android.basicandroidaccessibility.activity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.ImageButton
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.example.android.basicandroidaccessibility.R
 import com.example.android.basicandroidaccessibility.databinding.ActivityTemplateBinding
-
 
 
 class TemplateActivity : AppCompatActivity() {
@@ -21,6 +20,15 @@ class TemplateActivity : AppCompatActivity() {
     private var txtString: TextView? = null
     private var imageSave: ImageView? = null
     private var imageBack: ImageView? = null
+
+    // 四个文本字段
+    var etFirstName: EditText? = null
+    var etLastName:EditText? = null
+    var etEmail:EditText? = null
+    var etPassword:EditText? = null
+
+    // 校验标识符
+    var isAllFieldsChecked: Boolean = false
 
     private var _binding: ActivityTemplateBinding? = null
     private val binding get() = _binding!!
@@ -30,9 +38,16 @@ class TemplateActivity : AppCompatActivity() {
         _binding = ActivityTemplateBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // 绑定导航栏
         txtString = binding.txtString
         imageSave = binding.imageSave
         imageBack = binding.imageBack
+        // 绑定内容表单
+        etFirstName = binding.firstName
+        etLastName = binding.lastName
+        etEmail = binding.email
+        etPassword = binding.password
+
         // getting the bundle from the intent
         val bundle = intent.extras
         bundle!!.getString("key1")?.let { Log.d("接受参数", it) }
@@ -51,19 +66,47 @@ class TemplateActivity : AppCompatActivity() {
         //supportActionBar?.setLogo(R.drawable.logo_youtube)
 //        supportActionBar?.setDisplayUseLogoEnabled(true)
 
+        // 注册所有 EditText 字段。
+
+
         // 保存：点击事件
         imageSave?.setOnClickListener {
-            Log.d("保存", "保存的点击事件")
             Toast.makeText(this, "保存", Toast.LENGTH_SHORT).show();
+            isAllFieldsChecked = CheckAllFields()
+            if (isAllFieldsChecked){
+                Log.d("校验成功", "保存数据库中...")
+            }
         }
         // 返回：点击事件
         imageBack?.setOnClickListener {
             Toast.makeText(this, "返回", Toast.LENGTH_SHORT).show();
-            finish()
+            this.finish()
+            System.exit(0)
         }
     }
 
-
+    private fun CheckAllFields(): Boolean {
+        if (etFirstName?.length() == 0) {
+            etFirstName?.error = "This field is required"
+            return false;
+        }
+        if (etLastName?.length() == 0) {
+            etLastName?.error = "This field is required"
+            return false;
+        }
+        if (etEmail?.length() == 0) {
+            etEmail?.error = "Email is required"
+            return false;
+        }
+        if (etPassword?.length() == 0) {
+            etPassword?.error = "Password is required"
+            return false;
+        } else if (etPassword?.length()!! < 8) {
+            etPassword?.error = "Password must be minimum 8 characters"
+            return false
+        }
+        return true
+    }
 
 
 }
